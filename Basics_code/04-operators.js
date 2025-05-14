9d2d31012c4fce3211864c3e4ecce5f6ceeae24b
Samuel-Giraldo <samuelgozgiraldo1357@gmail.com>
2025-05-14 14:30:22 -0500

    Se sube 3 parte de roadmap

---
+++ Basics_code/04-operators.js
@@ -0,0 +1,65 @@
+// Operadores Arimeticos
+console.log('Operadores Arimeticos')
+
+let a = 5
+let b = 10
+
+console.log(a + b) // Suma
+console.log(a - b) // Resta
+console.log(a * b) // Multiplicacion
+console.log(a / b) // Division
+console.log(a % b) // Modulo
+console.log(a ** b) // Exponente 
+
+a++ // Incremento
+console.log(a) 
+
+b-- // Decremento 
+console.log(b)
+
+// Operadores de asignacion
+console.log('Operadores de asignacion') 
+
+let myvariable = 2
+console.log(myvariable)
+myvariable += 2
+console.log(myvariable)
+
+myvariable -= 2
+myvariable *= 2
+myvariable /= 2
+myvariable %= 2
+myvariable **= 2
+
+// Operadores de comparacion 
+console.log('Operadores de comparacion')
+
+console.log(a > b)
+console.log(a < b)
+console.log(a >= b) 
+console.log(a == b) 
+console.log(a == 6) // Igualdad por valor
+console.log(a == '6') // Igualdad por valor
+console.log(a == a)
+console.log(a === a) // Igualdad por identidad  (por tipo y valor)
+console.log(a === 6)
+console.log(a === '6')
+console.log(a != 6) // Negacion
+console.log(a !== '6')
+
+console.log('Comparaciones Absolutas')
+
+console.log(0 == false)
+console.log(1 == false)
+console.log(2 == false)
+console.log(0 == '')
+console.log(0 == ' ')
+console.log(0 == 'Hola')
+console.log(0 === '')
+console.log(undefined == null)
+console.log(undefined === null)
+
+
+
+// Truthy valores (valores verdaderos)
+
